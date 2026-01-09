6a85e314accc7b42bfbadc0c7ed88c35d2e10c63
Harley Torrisi <mail@harleycodes.com>
2026-01-09 17:37:19 +1000

    Update get-sonar-address.test.ts

--- tests/unit/get-sonar-address.test.ts
+++ tests/unit/get-sonar-address.test.ts
@@ -10,14 +10,12 @@ import { getSonarAddress } from '../../src/functions/get-sonar-address'
 let originalFetch: typeof fetch
 
 describe('getSonarAddress', () => {
-	// We mock getAppAddress to avoid testing its implementation here
-	mock.module('../../src/functions/get-app-address', () => ({
-		getAppAddress: (): Promise<string> => {
-			return Promise.resolve('127.0.0.1:9999')
-		}
-	}))
-
 	beforeEach(() => {
+		mock.module('../../src/functions/get-app-address', () => ({
+			getAppAddress: (): Promise<string> => {
+				return Promise.resolve('127.0.0.1:9999')
+			}
+		}))
 		originalFetch = globalThis.fetch
 	})
 
