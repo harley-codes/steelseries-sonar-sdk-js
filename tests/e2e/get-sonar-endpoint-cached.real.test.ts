b262782fb6b0d167bc0be191307db37e20bc3ac3
Harley Torrisi <mail@harleycodes.com>
2026-01-16 23:00:58 +1000

    Add missing test case for cached endpoint

--- tests/e2e/get-sonar-endpoint-cached.real.test.ts
+++ tests/e2e/get-sonar-endpoint-cached.real.test.ts
@@ -1,4 +1,4 @@
-import { afterEach, describe, expect, it } from 'bun:test'
+import { afterEach, describe, expect, it, spyOn } from 'bun:test'
 import { getAppEndpoint } from '@/functions/endpoint/get-app-endpoint'
 import { getSonarEndpointCached } from '@/functions/endpoint/get-sonar-endpoint-cached'
 
@@ -11,11 +11,22 @@ afterEach(() => {
 	logs.length = 0
 })
 
+const spy = spyOn(globalThis, 'fetch')
+
 describe('getSonarEndpointCached', () => {
 	it('returns sonarAddress', async () => {
 		const appAddress = await getAppEndpoint()
 		const sonarAddress = await getSonarEndpointCached(appAddress)
 		expect(sonarAddress).toBeString()
+		logs.push(`> sonar address: ${sonarAddress}`)
+	})
+
+	it('returns cached sonarAddress', async () => {
+		const appAddress = await getAppEndpoint()
+		await getSonarEndpointCached(appAddress)
+		const sonarAddress = await getSonarEndpointCached(appAddress)
+		expect(sonarAddress).toBeString()
+		expect(spy).toHaveBeenCalledTimes(1)
 		logs.push(`> sonar address: ${sonarAddress}`)
 	})
 })
