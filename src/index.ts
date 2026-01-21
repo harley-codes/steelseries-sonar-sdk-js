9d49cadf5df22779d5227e424dfd1eb523646ae4
Harley Torrisi <mail@harleycodes.com>
2026-01-21 15:26:03 +1000

    Update exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,41 @@
 export * from '@/enums'
 export * from '@/exceptions'
+
+// Audio
 export * from '@/functions/audio/get-audio-data-classic'
 export * from '@/functions/audio/get-audio-data-stream'
 export * from '@/functions/audio/get-audio-mode'
 export * from '@/functions/audio/set-audio-mode'
+export * from '@/functions/audio/set-channel-mute-classic'
+export * from '@/functions/audio/set-channel-mute-streamer'
+export * from '@/functions/audio/set-channel-volume-classic'
+export * from '@/functions/audio/set-channel-volume-streamer'
+
+// Chatmix
+export * from '@/functions/chatmix/get-chat-mix-state'
+export * from '@/functions/chatmix/set-chat-mix-balance'
+
+// Devices
+export * from '@/functions/devices/get-audio-devices'
+export * from '@/functions/devices/set-audio-device'
+
+// Endpoint
 export * from '@/functions/endpoint/get-app-endpoint'
 export * from '@/functions/endpoint/get-sonar-endpoint'
 export * from '@/functions/endpoint/get-sonar-endpoint-cached'
+
+// Profile
+export * from '@/functions/profile/get-channel-profiles'
+export * from '@/functions/profile/get-selected-profiles'
+export * from '@/functions/profile/set-selected-profile'
+
+// Types
+export * from '@/types/audio-device'
+export * from '@/types/changed-device'
+export * from '@/types/channel-volume-classic'
+export * from '@/types/channel-volume-streamer'
+export * from '@/types/channel-volume-streamer-path'
+export * from '@/types/channel-volumes-classic'
+export * from '@/types/channel-volumes-streamer'
+export * from '@/types/chat-mix-data'
+export * from '@/types/profile-option'
